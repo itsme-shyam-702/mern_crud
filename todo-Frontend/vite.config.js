a2a0f7a8d991162cdf7e150d5aa3e995e312cf09
Shyam <sssshyam702@gmail.com>
2025-09-30 17:28:29 +0530

    Update vite.config.js

--- todo-Frontend/vite.config.js
+++ todo-Frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: true // enables PostCSS so Tailwind works and lightningcss is disabled
+  }
 })
